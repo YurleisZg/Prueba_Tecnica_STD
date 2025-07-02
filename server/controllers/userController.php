917188531635cd1fb5fb3c8ba4c67b690733fb20
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 17:53:05 -0500

    ✨ feat: Update UserController to include getUsers and saveUser methods for user management

--- server/controllers/userController.php
+++ server/controllers/userController.php
@@ -1,9 +1,26 @@
 <?php
 
-require_once __DIR__ . '/../utils/response.php';
+require_once __DIR__ . '/../utils/Response.php';
 
 class UserController {
 
     private $dataFile = __DIR__ . '/../data/users.json';
 
+    public function getUsers() {
+        $users = json_decode(file_get_contents($this->dataFile), true);
+        jsonResponse($users);
+    }
+
+    public function saveUser($newUser) {
+        $users = json_decode(file_get_contents($this->dataFile), true);
+
+        $newUser['id'] = count($users) ? $users[count($users) - 1]['id'] + 1 : 1;
+        $newUser['createdAt'] = date('Y-m-d');
+
+        $users[] = $newUser;
+
+        file_put_contents($this->dataFile, json_encode($users, JSON_PRETTY_PRINT));
+
+        jsonResponse($users);
+    }
 }
