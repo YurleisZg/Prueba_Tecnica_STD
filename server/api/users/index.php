1b7ff4142a7915d3732da4d0b5ae8c65a1a7c2fe
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 18:02:43 -0500

    ✨ feat: Implement API endpoint for user management with GET and POST methods

---
+++ server/api/users/index.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/../../controllers/UserController.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST');
+header('Content-Type: application/json');
+
+$controller = new UserController();
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'GET':
+        $controller->getUsers();
+        break;
+
+    case 'POST':
+        $input = file_get_contents('php://input');
+        $newUser = json_decode($input, true);
+
+        if (!$newUser) {
+            http_response_code(400);
+            echo json_encode(['error' => 'Invalid JSON']);
+            exit;
+        }
+
+        $controller->saveUser($newUser);
+        break;
+
+    default:
+        http_response_code(405);
+        echo json_encode(['error' => 'Method Not Allowed']);
+        break;
+}
