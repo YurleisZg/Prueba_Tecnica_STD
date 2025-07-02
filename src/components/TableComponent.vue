bf8b9d474bc223200129e88606208ddbfbd6a7a0
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 14:28:16 -0500

    ✨ feat: Replace HelloWorld component with TableComponent and remove unused styles

---
+++ src/components/TableComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Birth Date</th>
+                <th>Created At</th>
+                <th>Role</th>
+            </tr>
+        </thead>
+    </table>
+
+</template>
+
+<script lang="ts">
+</script>
+
+<style scoped>
+</style>
