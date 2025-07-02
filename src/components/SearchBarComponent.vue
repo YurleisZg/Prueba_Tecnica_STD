0a1deb3a2ca480768593a5f55b426523e8de4227
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 15:13:37 -0500

    ✨ feat: Add SearchBarComponent for user search functionality

---
+++ src/components/SearchBarComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="search-bar">
+        <input id="search" v-model="search" placeholder="Buscar por nombre" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const search = ref('');
+
+</script>
+
+<style scoped>
+</style>
