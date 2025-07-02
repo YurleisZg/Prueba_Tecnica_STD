627fabe4790254c2ea35f6eff45d43940cfb5e17
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 15:47:51 -0500

    ✨ feat: Implement saveUser function to add new users to local storage

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -4,3 +4,9 @@ import type { User } from '../models/user';
 export const getUsers = (): User[] => {
     return userData;
 };
+
+export const saveUser = (user: User): void => {
+    const users = getUsers();
+    users.push(user);
+    localStorage.setItem('users', JSON.stringify(users));
+};
