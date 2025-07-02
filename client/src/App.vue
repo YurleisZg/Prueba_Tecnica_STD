4bcd4b712faccf705b2e47aa4862331c4ec50c6f
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 18:23:55 -0500

    ✨ feat: Add footer component with author information in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,16 @@ import {RouterView} from 'vue-router';
 
 <template>
   <RouterView />
+  <footer class="footer">
+    <p>Author: Yurleis Zuluaga, 2025</p>
+</footer>
 </template>
 
 <style scoped>
+.footer {
+    margin-top: 20px;
+    text-align: center;
+    font-size: 0.8em;
+    color: #999;
+}
 </style>
