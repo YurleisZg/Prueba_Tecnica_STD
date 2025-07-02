9e37bf661f955620c91da7a5e927d54fcaa0d615
Yurleis Zuluaga <yzuluagag1@unicartagena.edu.co>
2025-07-02 16:02:57 -0500

    ✨ feat: Enhance NewUserComponent with form labels and improve user experience; update userService to use consistent storage key

--- src/components/NewUserComponent.vue
+++ src/components/NewUserComponent.vue
@@ -1,11 +1,20 @@
 <template>
 <div class="scoped">
+    <button @click="$router.push('/')" class="back-button">Regresar</button>
+    <h1 class="title">Agregar Nuevo Usuario</h1>
+    <p class="description">Por favor, complete el siguiente formulario para agregar un nuevo usuario.</p>
     <form @submit.prevent="saveNewUser">
-        <input type="number" v-model="newUser.id" placeholder="Identificación" required />
+        <label for="id">Identificación:</label>
+        <input type="number" v-model="newUser.id" placeholder="Identificación" required/>
+        <label for="name">Nombre:</label>
         <input type="text" v-model="newUser.name" placeholder="Nombre" required />
+        <label for="email">Correo Electrónico:</label>
         <input type="email" v-model="newUser.email" placeholder="Correo Electrónico" required />
+        <label for="birthDate">Fecha de Nacimiento:</label>
         <input type="date" v-model="newUser.birthDate" placeholder="Fecha de Nacimiento" required />
+        <label for="createdAt">Fecha de Creación:</label>
         <input type="date" v-model="newUser.createdAt" placeholder="Fecha de Creación" required />
+        <label for="role">Cargo:</label>
         <input type="text" v-model="newUser.role" placeholder="Cargo" required />
         <button type="submit">Guardar Usuario</button>
     </form>
@@ -56,5 +65,23 @@ button {
     border: none;
     border-radius: 4px;
     cursor: pointer;
+}
+.title {
+    font-size: 24px;
+    text-align: center;
+    margin-bottom: 20px;
+}
+.description {
+    text-align: center;
+    margin-bottom: 20px;
+}
+.back-button {
+    background-color: #dadde3;
+    color: #0c0c0c;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 4px;
+    cursor: pointer;
+    margin-bottom: 20px;
 }
 </style>
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,18 @@
 import userData from '../data/users.json';
 import type { User } from '../models/user';
 
+const storagekey = 'users-data';
+
 export const getUsers = (): User[] => {
+    const storedUsers = localStorage.getItem(storagekey);
+    if (storedUsers) {
+        return JSON.parse(storedUsers)
+    }   
     return userData;
 };
 
 export const saveUser = (user: User): void => {
     const users = getUsers();
     users.push(user);
-    localStorage.setItem('users', JSON.stringify(users));
+    localStorage.setItem(storagekey, JSON.stringify(users));
 };
